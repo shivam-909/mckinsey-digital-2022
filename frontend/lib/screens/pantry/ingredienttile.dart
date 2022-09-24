9a95878a785b666dfd5b38938292b342fe9edb9a
muhsin <muhsin.ma.mohamed@gmail.com>
2022-09-24 23:33:52 +0100

    semicolon

--- frontend/lib/screens/pantry/ingredienttile.dart
+++ frontend/lib/screens/pantry/ingredienttile.dart
@@ -4,32 +4,29 @@ import 'package:frontend/util/palette.dart';
 import 'package:frontend/util/utils.dart';
 
 class IngredientTile extends StatefulWidget {
-    const IngredientTile({super.key, required this.item});
+  const IngredientTile({super.key, required this.item});
 
-    final PantryItem item;
+  final PantryItem item;
 
-    @override
-    State<IngredientTile> createState() => _IngredientTileState();
+  @override
+  State<IngredientTile> createState() => _IngredientTileState();
 }
 
 class _IngredientTileState extends State<IngredientTile> {
-    @override
-    Widget build(BuildContext context) {
-        return Row(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-                Container(
-                    decoration: BoxDecoration(borderRadius: BorderRadius.circular(12)),
-                    child: SizedBox(
-                        height: 100,
-                        width: 100,
-                        child: Image.network(
-                            widget.item.imageUrl,
-                            fit: BoxFit.cover,
-                        ),
-                    ),
-                )
-            ]
-        )
-    }
+  @override
+  Widget build(BuildContext context) {
+    return Row(mainAxisSize: MainAxisSize.min, children: [
+      Container(
+        decoration: BoxDecoration(borderRadius: BorderRadius.circular(12)),
+        child: SizedBox(
+          height: 100,
+          width: 100,
+          child: Image.network(
+            widget.item.imageUrl,
+            fit: BoxFit.cover,
+          ),
+        ),
+      )
+    ]);
+  }
 }
